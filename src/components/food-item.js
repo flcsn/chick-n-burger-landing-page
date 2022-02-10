70fb6e66f752df58f255a96ae1eecacf79a32331
flcsn <flacson98@gmail.com>
2022-02-10 21:30:16 +0800

    added basic styling for food items

--- src/components/food-item.js
+++ src/components/food-item.js
@@ -5,13 +5,13 @@ const FoodItem = () => {
   return (
     <div className='food-item'>
       <div className='food-image'>
-        <img src={Chicken1}></img>
+        <img src={Chicken1} alt='image of food'/>
       </div>
       <div className='food-description'>
-        sample description
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non fringilla erat. Vestibulum elementum enim.
       </div>
-      <div className='order-food-btn'>
-        <button>Order now</button>
+      <div className='order-food-btn-container'>
+        <button className='order-food-btn'>Order now</button>
       </div>
     </div>
   )
