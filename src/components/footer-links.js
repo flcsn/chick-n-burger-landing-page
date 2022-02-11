e517acec71395e735902fab3e3e97c331a9db103
flcsn <flacson98@gmail.com>
2022-02-11 21:41:18 +0800

    extracted trademark and links to their own component

---
+++ src/components/footer-links.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const FooterLinks = () => {
+  return (
+    <ul className='footer-links'>
+      <li>
+        <a href='#'>Contact Us</a>
+      </li>
+      <li>
+        <span className='footer-divider'>|</span>
+      </li>
+      <li>
+        <a href='#'>Site Map</a>
+      </li>
+      <li>
+        <span className='footer-divider'>|</span>
+      </li>
+      <li>
+        <a href='#'>Privacy Policy</a>
+      </li>
+    </ul>
+  )
+}
+
+export default FooterLinks
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,28 +1,12 @@
 import React from 'react'
+import FooterTrademark from './footer-trademark'
+import FooterLinks from './footer-links'
 
 const Footer = () => {
   return (
     <div className='footer'>
-      <div className='footer-trademark'>
-        <p>© 2022 Chick&apos;N&apos;Burgers. All rights reserved.</p>
-      </div>
-      <ul className='footer-links'>
-        <li>
-          <a href='#'>Contact Us</a>
-        </li>
-        <li>
-          <span className='footer-divider'>|</span>
-        </li>
-        <li>
-          <a href='#'>Site Map</a>
-        </li>
-        <li>
-          <span className='footer-divider'>|</span>
-        </li>
-        <li>
-          <a href='#'>Privacy Policy</a>
-        </li>
-      </ul>
+      <FooterTrademark />
+      <FooterLinks />
     </div>
   )
 }
