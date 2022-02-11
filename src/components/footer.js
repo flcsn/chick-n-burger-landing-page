7fbfb1ce410a1fd82685d02b28c4738d3b44f563
flcsn <flacson98@gmail.com>
2022-02-11 22:01:31 +0800

    added social media icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import FooterSocials from './footer-socials'
 import FooterTrademark from './footer-trademark'
 import FooterLinks from './footer-links'
 
 const Footer = () => {
   return (
     <div className='footer'>
+      <FooterSocials />
       <FooterTrademark />
       <FooterLinks />
     </div>
