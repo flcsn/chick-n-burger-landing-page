b2056b864fda544b9f5ced62f6b2701d2555318d
flcsn <flacson98@gmail.com>
2022-02-11 17:10:21 +0800

    added food title and price

--- src/components/food-item.js
+++ src/components/food-item.js
@@ -6,6 +6,12 @@ const FoodItem = ({ image }) => {
       <div className='food-image'>
         <img src={image} alt='image of food'/>
       </div>
+      <div className='food-title'>
+        <p>Chicken</p>
+      </div>
+      <div className='food-price'>
+        <span>PhP 500.00</span>
+      </div>
       <div className='food-description'>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non fringilla erat. Vestibulum elementum enim.
       </div>
